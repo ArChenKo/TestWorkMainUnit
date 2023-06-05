faae404e80c3c9b3e1fcce708a3fcac5ab1f7159
Arslan <arslan.chen@mail.ru>
2023-06-05 21:03:48 +0400

    final correction in program added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿// 
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
 string Parametr(string message) // метод для ввода значений с клавиатуры
 {
     Console.Write(message);
@@ -11,7 +15,6 @@ string Parametr(string message) // метод для ввода значений
     }
     return value;
 }
-
 string [] FillArray(int sizeArray) // метод заполнения массива значениями
 {
     string [] array = new string[sizeArray];
@@ -19,25 +22,32 @@ string [] FillArray(int sizeArray) // метод заполнения масси
         array[i]=Parametr("Введите следующий элемент списка:");
     return array;
 }
-
 string [] SelectionArray(string [] array) // метод создания строкового массива с отобранными значениями
 {
     string list = string.Empty;
-    //int count = 0;
+    int count = 0;
+    string ch = "/";
     for (int i = 0; i < array.Length; i++)
     {
         if(array[i].Length<=3)
         {
-            list += array[i]+"/";
-            //count++;
+            list += array[i]+ch;
+            count++;
         }
     }
-    //string[]matrix = new string[count];
-    string [] matrix = list.Split("/");
+    string[]matrix = new string[count];
+    int k=0;
+    for (int i = 0; i < list.Length; i++)
+    {
+        if(list[i]==ch[0])
+            k++;
+        else
+            matrix[k]+=list[i];
+    }
     return matrix;
 }
 
 string[]listArray=FillArray(5);
 string[]array=SelectionArray(listArray);
-Console.WriteLine($"[{String.Join(",",listArray)}]");
-Console.WriteLine($"[{String.Join(",",array)}]");
+Console.WriteLine($"[{String.Join(",",listArray)}] [{listArray.Length}]");
+Console.WriteLine($"[{String.Join(",",array)}] [{array.Length}]");
